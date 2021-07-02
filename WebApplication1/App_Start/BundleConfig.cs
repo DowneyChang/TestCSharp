683371546543a843eaa59e28bde9a69393d3eeb7
Downey <downey.c1030@gmail.com>
2021-07-02 17:25:59 +0800

    0702 Datepicker 套件

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace WebApplication1
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/js/jquery-ui").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -21,6 +25,9 @@ namespace WebApplication1
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/css/jquery-ui").Include(
+                "~/Content/jquery-ui*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
